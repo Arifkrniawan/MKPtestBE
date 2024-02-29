c49c22d4013a32e0bd56a47be1097b3ac9c46af2
arifkrniawan <arif.kr96@gmail.com>
2024-02-29 14:00:27 +0700

    1st commit

---
+++ FundamentalTest_1.go
@@ -0,0 +1,32 @@
+
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	if n <= 3 {
+		return true
+	}
+
+	if n%2 == 0 || n%3 == 0 {
+		return false 
+	}
+
+	i := 5 
+	for i*i <= n {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func prime(n int) {
+	for := 2; i <= limit; i++ {
+		if isPrime(i){
+			fmt.Print(i," ")
+		}
+	}
+	fmt.Println()
+}
